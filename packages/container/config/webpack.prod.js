62f7080c34c5825cebcbfedeabbb4ff2deb53b12
Tor Fahsing <torfah@hotmail.com>
2021-11-08 14:46:31 +0100

    Add missing remote

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -14,7 +14,8 @@ const prodConfig = {
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
-                marketing: `marketing@${domain}/marketing/remoteEntry.js`
+                marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+                auth: 'auth@http://localhost:8082/remoteEntry.js'
             },
             shared: packageJson.dependencies
         })
