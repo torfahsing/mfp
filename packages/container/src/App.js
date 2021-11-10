33864870d479d7955631cb60bf4756f098882f12
Tor Fahsing <torfah@hotmail.com>
2021-11-10 13:03:56 +0100

    Remove console.log

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -17,7 +17,6 @@ const generateClassName = createGenerateClassName({
 const history = createBrowserHistory();
 const App = () => {
     const [isSignedIn, setIsSignedIn] = useState(false);
-    console.log('So yeah');
     useEffect(() => {
         if (isSignedIn) {
             history.push('/dashboard');
